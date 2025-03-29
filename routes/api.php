51a53c66954bc1547e31cb6d7f6fb287a089a35b
Jahongir <alimovj727@gmail.com>
2025-03-29 05:11:56 +0500

    🏗 Setup UserController

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\UserController as UserV1;
+use App\Http\Controllers\API\V2\UserController as UserV2;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::get('/users', [UserV1::class, 'index']);
+});
+
+Route::prefix('v2')->group(function () {
+    Route::get('/users', [UserV2::class, 'index']);
+});
