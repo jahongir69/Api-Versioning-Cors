7baa381ced58cba4847fbff8c4c5499289a46ed8
Jahongir <alimovj727@gmail.com>
2025-03-29 05:19:37 +0500

    🎯 Implement v1/users API

--- app/Http/Controllers/API/V2/UserController.php
+++ app/Http/Controllers/API/V2/UserController.php
@@ -1,11 +1,14 @@
 <?php
-
 namespace App\Http\Controllers\API\V2;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(User::select('id', 'name', 'email', 'role')->get());
+    }
 }
